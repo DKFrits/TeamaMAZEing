9c1d2db5eaa2b790ede92da41d639949173c5ad0
Niels <engelhardniels@gmail.com>
2021-03-18 13:49:39 +0100

    NES func added

--- Donderdag18-03Level/Assets/Characters/Character/SC_FPSController.cs
+++ Donderdag18-03Level/Assets/Characters/Character/SC_FPSController.cs
@@ -11,7 +11,8 @@ public class SC_FPSController : MonoBehaviour
 
     public int health;
 
-    public Text txt;
+    public Text healthTxt;
+    public Text controllerTxt;
 
     public float walkingSpeed = 7.5f;
     public float runningSpeed = 11.5f;
@@ -27,6 +28,8 @@ public class SC_FPSController : MonoBehaviour
 
     [HideInInspector]
     public bool canMove = true;
+
+    private bool hasController;
 
     void Start()
     {
@@ -103,12 +106,20 @@ public class SC_FPSController : MonoBehaviour
     public void UpdateHealthBar()
     {
         var newText = "health: " + health;
-        txt.GetComponent<Text>().text = newText;
+        healthTxt.GetComponent<Text>().text = newText;
     }
 
     private void HUDInit()
     {
+        hasController = false;
         var newText = "health: " + health;
-        txt.GetComponent<Text>().text = newText;
+        healthTxt.GetComponent<Text>().text = newText;
+    }
+
+    public void AddController()
+    {
+        var newText = "Controller: picked up";
+        controllerTxt.GetComponent<Text>().text = newText;
+        bool hasController = true;
     }
 }
---
+++ Donderdag18-03Level/Assets/PickUps/Controller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : Interactable
+{
+
+    public readonly string itemName = "NEScontroller";
+
+    private AudioSource audioSource;
+    public AudioClip itemPickUpSound;
+
+    public GameObject player;
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        PickUp();
+    }
+
+    public virtual void PickUp()
+    {
+        Debug.Log("picking up " + itemName);
+        AddControllerToPlayer();
+        PlayPickupSound();
+        Destroy(gameObject);
+
+    }
+
+    void PlayPickupSound()
+    {
+        var gameManager = GameObject.Find("GameManager");
+        var audioSource = gameManager.GetComponent<AudioSource>();
+        audioSource.clip = itemPickUpSound;
+        audioSource.Play();
+    }
+
+    private void AddControllerToPlayer()
+    {
+        var playerScript = player.GetComponent<SC_FPSController>();
+        playerScript.AddController();
+    }
+
+}
