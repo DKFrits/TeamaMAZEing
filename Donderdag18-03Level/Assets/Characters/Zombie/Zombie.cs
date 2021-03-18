e127ec00c5a474d1b34d6ed236ea65c43402f174
Niels <engelhardniels@gmail.com>
2021-03-18 10:03:40 +0100

    test

---
+++ Donderdag18-03Level/Assets/Characters/Zombie/Zombie.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zombie : SimpleZombieFSM
+{
+
+    public enum ZombieState
+    {
+        None,
+        Idle,
+        Walk,
+        Attack
+    }
+
+    public ZombieState curState;
+
+
+
+    protected override void Initialize()
+    {
+
+    }
+
+    //Update each frame
+    protected override void FSMUpdate()
+    {
+        switch (curState)
+        {
+            case ZombieState.Idle: UpdateIdleState(); break;
+            case ZombieState.Walk: UpdateWalkState(); break;
+            case ZombieState.Attack: UpdateAttackState(); break;
+        }
+
+    }
+
+    private void UpdateIdleState()
+    {
+
+    }
+
+    private void UpdateWalkState()
+    {
+
+    }
+
+    private void UpdateAttackState()
+    {
+
+    }
+}
